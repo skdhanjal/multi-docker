6bbfe44f64e42f7264b68f6aa8cb7b2d85ff121e
Shashi Kumar <shashi.kumar@walmart.com>
2023-04-25 20:11:26 +0530

    update client

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Fib Calculator</h1>
+          <h1>Fib Calculator Kubernetes (Deployemnt)</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
